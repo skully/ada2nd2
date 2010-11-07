e6c0166c0b4e9c21fd9f83ddbad7aa46daaa501c
skully <skully@elte.hu>
2010-11-07 16:36:38 +0100

    kesz a binsearch, majdnem

--- arraygen.adb
+++ arraygen.adb
@@ -59,12 +59,73 @@ end insert;
 
 function findefficiency(A:arrayrecord; E:element) return integer is
 
+howmany_loops:integer:=0;
+found    :  boolean;
+Index    :  integer;
+left  : integer := a.items'First;    --elso
+right : integer; --utoso utani
 begin
+     
+          declare
+          i:integer:=a.items'last;
+          begin
+               --megadja az utolso nem extremal-t
+               while i>=a.items'first and then a.items(i) = extremal
+               loop
+                    i:=i-1;
+               end loop;
+               right:=i+1;
+
+          end;
+          
+
+     if E < a.items (Left) then              --elso hellyel hasonlit
+          Index := Left - 1;                         --0. helyre szurna be
+          Found := False;                            --nem talalt
+     else                                          
+          loop                                       
+               declare
+                    Center    : constant Integer   :=
+                    Left + (Right - Left) / 2;        --kozepso elem indexe
+                    Candidate : constant Element :=
+                    a.items (Center);                --kozepso elem
+                  
+               begin
+                    howmany_loops:=howmany_loops+1;
+
+                    if E = Candidate then           --ha egyenlo
+                         Index := Center;                  --index
+                         Found := True;                    --megtalaltuk
+                         exit;                             --kilepes
+                    end if;
+
+                    if Right - Left <= 1 then            --ha a ket index kulonbsege negativ
+                         Index := Center;                  
+                         Found := False;                   --nincs meg
+                         exit;
+                    elsif E < Candidate then        --meg nincs meg, mert az elso reszben van
+                         Right := Center;	                 --a left es a center kozott keresunk tovabb
+                    else
+                         Left := Center;                   --kulonben a hatso reszben van es a center es a right kozott keresunk tovabb
+                    end if;
+                    ada.text_io.put_line(integer'image(howmany_loops));
+                end;
+            
+          end loop;                                  --ciklus vege
+     end if;
+     if not found 
+     then 
+          howmany_loops:=-1;
+     end if;
+
+      return howmany_loops;
 
 
 
-null;
-return 0;
+
+
+
+
 
 
 end findefficiency;
--- tombos.adb
+++ tombos.adb
@@ -7,7 +7,7 @@ package int_arraygen is new arraygen(Integer, "<", 0); use int_arraygen;
 --size: integer;
 
 
-v:arrayrecord(5);
+v:arrayrecord(100);
 
 
 
@@ -15,10 +15,17 @@ v:arrayrecord(5);
 
 begin
 
-Insert(v, 2);
-insert(v,4);
-
+Insert(v,2);
+--insert(v,4);
 insert(v,3);
+insert(v,9);
+insert(v,13);
+insert(v,1337);
+insert(v,37);
+insert(v,42);
+
+ada.text_io.put_line("A ciklus ennyiszer fut le: " &integer'image(findefficiency(v,4)));
+
 
 
 end tombos;
